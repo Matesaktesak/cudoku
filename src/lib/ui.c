ef327e369fe7116b815ef7ca09013cfd898fa735
Matyáš Levíček <46134697+Matesaktesak@users.noreply.github.com>
2023-11-27 14:20:59 +0000

    added save playfield function

--- src/lib/ui.c
+++ src/lib/ui.c
@@ -16,7 +16,17 @@ void clearInput(){
     while((c = getchar()) != '\n' && c != EOF);
 }
 
-FILE* getFile(){
+int savePlayfield(Cell* p[], FILE* f){
+    for(int i = 0; i < 9 * 9; i++){
+        Cell c = **(p + i);
+        if(cellSolved(&c)) fprintf(f, "%d,%d,%d\n", c.x, c.y, cellValue(&c));
+    }
+    fclose(f);
+
+    return 1;
+}
+
+FILE* getFile(int writing){
     printf("Enter filename: ");
     char path[100] = "./saves/";
     do{
@@ -29,9 +39,11 @@ FILE* getFile(){
 
     strcat(path, ".cudoku");
 
-    FILE* f = fopen(path, "r");
-    if(!f){
+    FILE* f = fopen(path, writing ? "w" : "r");
+    if(!f && !writing){
         printf("Failed to open file\n");
-        return getFile();
-    }    
+        return getFile(0);
+    }
+
+    return f;
 }
--- src/lib/ui.h
+++ src/lib/ui.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "./cell.h"
 
 /*
     @brief Gets a number from stdin
@@ -14,14 +15,23 @@ int getNumber(char* message);
 
 /*
     @brief Gets a file name from stdin and returns a file pointer to the file
+    @param writing: 0 if file must exist and is opened only for reading, 0 if not
     @return file pointer to file
 */
-FILE* getFile();
+FILE* getFile(int writing);
 
 /*
     @brief Clears stdin
     @return void
 */
 void clearInput();
+
+/*
+    @brief Saves a playfield to a file
+    @param p: playfield to save
+    @param f: file pointer to file to save to
+    @return int: 1 if successful, 0 if not
+*/
+int savePlayfield(Cell* p[], FILE* f);
 
 #endif //CUDOKU_UI_H
